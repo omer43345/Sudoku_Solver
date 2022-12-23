9fe664a396c7c6bf51fa1e97b50bf4d10a232607
omer hadad <omer.hadad1@gmail.com>
2022-12-23 16:55:13 +0200

    add board builder class and added custom exception for the size of the string of the sudoku

---
+++ Sudoku_Solver/Exceptions/InvalidSudokuBoardSize.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku_Solver.Exceptions
+{
+    public class InvalidSudokuBoardSize : Exception
+    {
+        public InvalidSudokuBoardSize(int size) : base("Invalid sudoku board size, string length must be rootable perfectly twice, but the size was " + size)
+        {
+        }
+        public InvalidSudokuBoardSize(string message) : base(message)
+        {
+        }
+        public InvalidSudokuBoardSize(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+}
